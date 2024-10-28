4070c82b36cd99287b82339b2156178c3b8b56f7
Putranta Aswintama <putrantaaswintama21@gmail.com>
2024-10-28 13:23:32 +0700

    landing

--- resources/views/livewire/components/landing-navbar.blade.php
+++ resources/views/livewire/components/landing-navbar.blade.php
@@ -73,6 +73,6 @@
                 <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
             </svg>
         </label>
-        <a class="btn btn-primary btn-sm">Login</a>
+        <a class="btn btn-primary btn-sm" href="{{ url('login') }}">Login</a>
     </div>
 </div>
