84359b9dec67488c70562a51ee091b4b6eca8b1a
Putranta Aswintama <putrantaaswintama21@gmail.com>
2024-10-28 11:58:56 +0700

    Update landing-navbar.blade.php

--- resources/views/livewire/components/landing-navbar.blade.php
+++ resources/views/livewire/components/landing-navbar.blade.php
@@ -73,29 +73,6 @@
                 <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
             </svg>
         </label>
-        <div class="dropdown dropdown-end">
-            <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-                <div class="indicator">
-                    <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-                        <div class="w-7 rounded-full">
-                            <img alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <ul tabindex="0"
-                class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                <li><a>Setting</a></li>
-                <li>
-                    <form method="POST" action="{{ route('logout') }}" x-data>
-                        @csrf
-                        <button type="submit">
-                            {{ __('Log Out') }}
-                        </button>
-                    </form>
-                </li>
-            </ul>
-        </div>
+        <a class="btn btn-primary btn-sm">Login</a>
     </div>
 </div>
