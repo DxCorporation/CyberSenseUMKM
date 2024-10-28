1e22d7072c5c5bb34b48dc805417ba01c7f49275
Putranta Aswintama <putrantaaswintama21@gmail.com>
2024-10-28 20:31:18 +0700

    Update pengembang.blade.php

--- resources/views/livewire/pengembang.blade.php
+++ resources/views/livewire/pengembang.blade.php
@@ -1,3 +1,11 @@
 <div class="min-h-[100vh] overflow-hidden pb-40 pt-10 px-8">
-    <h1>Pengembang</h1>
+    <h1 class="text-5xl font-bold text-center">
+        <span
+            class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-purple-500 via-green-400 to-cyan-500">
+            Pengembang
+        </span>
+    </h1>
+    <p class="text-center mt-8">
+        Website ini dibuat sebagai Projek Akhir Mata Kuliah Sistem Pakar Fakultas Informatika Universitas Janabadra 2024
+    </p>
 </div>
